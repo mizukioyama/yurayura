86a62db7d93f170b8649fe389efba632e4e27234
oy-mizuki <o.mizuki.art@gmail.com>
2026-09-13 17:49:21 +0900

    Center menu borders and tighten label spacing

--- assets/js/allmenu.js
+++ assets/js/allmenu.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", async () => {
    */
 
   const PARTS_PATH = "./assets/parts/";
-  const PARTS_VERSION = "20260912-hamburger-menu-unified";
+  const PARTS_VERSION = "20260913-menu-border-center-spacing-v2";
 
   const selectors = {
     header: "#js-header",
